0cc90b95123b1722cf41b0820b39de48e67c6cda
smiecj <459559238@qq.com>
2022-08-11 11:39:40 +0800

    fix-staticServicePath

--- src/browser/render-app.ts
+++ src/browser/render-app.ts
@@ -20,13 +20,16 @@ export async function renderApp(opts: IClientAppOpts) {
   opts.extensionDir = opts.extensionDir || process.env.EXTENSION_DIR;
   opts.injector = injector;
   opts.wsPath = process.env.WS_PATH || window.location.protocol == 'https:' ? `wss://${hostname}:${serverPort}` : `ws://${hostname}:${serverPort}`;
+
+  opts.extWorkerHost = opts.extWorkerHost || process.env.EXTENSION_WORKER_HOST || `http://${hostname}:${staticServerPort}/worker-host.js`;
+  opts.staticServicePath = `http://${hostname}:${serverPort}`;
+
   const NB_PREFIX = process.env.NB_PREFIX;
   if (NB_PREFIX != "") {
     opts.wsPath = opts.wsPath + NB_PREFIX;
+    opts.staticServicePath = opts.staticServicePath + NB_PREFIX;
   }
-  console.log("[test] wspath: " + opts.wsPath);
-  opts.extWorkerHost = opts.extWorkerHost || process.env.EXTENSION_WORKER_HOST || `http://${hostname}:${staticServerPort}/worker-host.js`;
-  opts.staticServicePath = `http://${hostname}:${serverPort}`;
+
   const anotherHostName = process.env.WEBVIEW_HOST || hostname;
   opts.webviewEndpoint = `http://${anotherHostName}:${webviewEndpointPort}/webview`;
   opts.layoutComponent = ToolbarActionBasedLayout;
