0772f2397e1b1651a5ffc8d291f2531d33848a3e
smiecj <459559238@qq.com>
2022-08-30 18:34:44 +0800

    change default WORKSPACE_DIR

--- src/node/start-server.ts
+++ src/node/start-server.ts
@@ -14,7 +14,7 @@ export async function startServer(arg1: NodeModule[] | Partial<IServerAppOpts>) 
   const deferred = new Deferred<http.Server>();
   process.env.EXT_MODE = 'js';
   const port = process.env.IDE_SERVER_PORT || 8000;
-  const workspaceDir = process.env.WORKSPACE_DIR || path.join(__dirname, '../../workspace');
+  const workspaceDir = process.env.WORKSPACE_DIR || '/home/jovyan';
   const extensionDir = process.env.EXTENSION_DIR || path.join(__dirname, '../../extensions');
   const extensionHost = process.env.EXTENSION_HOST_ENTRY || 
   process.env.NODE_ENV === 'production' ? path.join(__dirname, '..', '..', 'hosted/ext.process.js') : path.join(__dirname, '..', '..', 'hosted/ext.process.js');
